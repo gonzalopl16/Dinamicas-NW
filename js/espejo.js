a91e04a315a3ebc34e6eea0ac60ab5fef401416f
Gonzalo <renovado16@gmail.com>
2022-09-01 21:18:20 -0500

    Actualizando lista de Dinamicas y añadiendo espaciado

---
+++ js/espejo.js
@@ -0,0 +1,48 @@
+datos={
+    palabra:''
+}
+let palabra = document.querySelector('#palabra');
+let reducio = document.querySelector('.reducio');
+
+palabra.addEventListener('input', leer);
+
+reducio.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    const {palabra} = datos;
+    if(palabra === ''){
+        mostrar("Debe llenar los campos")
+    }else{
+        let a = intercalar(palabra);
+        mostrar(a);
+    }
+})
+
+function leer(evento){
+    datos[evento.target.id] = evento.target.value;
+    console.log(datos);
+}
+
+function intercalar(palabra){
+    let acum = "";
+    for(let i = 0; i <= palabra.length-1; i++){
+        acum += palabra.charAt(i);
+    }
+    for(let i = palabra.length-1; i >= 0; i--){
+        acum += palabra.charAt(i);
+    }
+        
+    return acum;
+}
+
+function mostrar(mensaje){
+    const error = document.createElement('pre');
+    error.textContent = mensaje;
+    error.classList.add('alert');
+    error.classList.add('alert-warning');
+    formulario.appendChild(error);
+    //Set Timeout
+    setTimeout(() => {
+        error.remove();
+    }, 10000);
+}
