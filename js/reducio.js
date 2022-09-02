1deaae7003f3d1d6756ca35f50e558bf92f4495a
Gonzalo <renovado16@gmail.com>
2022-09-01 20:07:09 -0500

    añadiendo ascendio y reducio

---
+++ js/reducio.js
@@ -0,0 +1,49 @@
+datos={
+    palabra:''
+}
+let palabra = document.querySelector('#palabra');
+let reducio = document.querySelector('.reducio');
+
+palabra.addEventListener('input', leer);
+
+reducio.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    const {palabra} = datos;
+    if(palabra === ''){
+        mostrar("Debe llenar los campos")
+    }else{
+        let a = intercalar(palabra);
+        mostrar(a);
+    }
+})
+
+function leer(evento){
+    datos[evento.target.id] = evento.target.value;
+    console.log(datos);
+}
+
+function intercalar(palabra){
+    let acum = "";
+    for (let i = palabra.length - 1; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            acum += palabra.charAt(j);
+        }
+        acum += "\n";
+    }
+    return acum;
+}
+
+function mostrar(mensaje){
+    const error = document.createElement('pre');
+    error.textContent = mensaje;
+    
+    error.classList.add('alert');
+    error.classList.add('alert-warning');
+    formulario.appendChild(error);
+
+    //Set Timeout
+    setTimeout(() => {
+        error.remove();
+    }, 10000);
+}
